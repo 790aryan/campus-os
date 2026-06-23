0a747a0ae193236c2f7bdcb2fcc94f127f3bb2f4
Aryan Gupta <guptaarayan929@gmail.com>
2026-06-23 21:13:48 +0530

    Fix Render trust proxy

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ import certificateRoutes from "./routes/certificateRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
 
 export const app = express();
-
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
 app.use(express.json({ limit: "2mb" }));
